4578ee6e0946cdaeee4db0a9dc1f5ea48b8437cb
coffeecookey <tanisha.ojha2004x2@gmail.com>
2026-06-19 17:33:21 +0530

    dataclasses

--- README.md
+++ README.md
@@ -7,19 +7,24 @@
 root/
 ├── main.py
 ├── config.py
+├── requirements.txt
+├── types.py
+│
 ├── models/
 │   ├──      
 │   ├──          
 │   ├──           
-│   └──      
+│   └── 
+│     
 ├── pipeline/
 │   ├── text_to_sql.py    
-│   ├── parser.py         
-│   └──
-│── schemas/
-│   ├──
-│   ├──
-│   └── 
+│   ├── parser.py 
+│   ├── codebert.py
+│   ├── error_checker.py
+│   ├── input.py
+│   ├── llm_checker.py        
+│   └── encoder.py
+│
 └── artifacts/ (not committed in this repo but locally stored)
     ├──unixcoder
     │  ├──config.json
--- types.py
+++ types.py
@@ -1,3 +1,56 @@
 '''
 Python dataclasses for clarity
 '''
+from dataclasses import dataclass
+
+# prompt (from database)
+@dataclass
+class input_prompt:
+    prompt : str
+
+# Output of text_to_sql
+@dataclass
+class sql_result:
+    prompt: str
+    sql_query: str
+
+# Output of parser.py
+@dataclass
+class parser_result:
+    sql_query: str
+    ast: object
+    parse_success: bool
+
+# Output of error_checker.py
+@dataclass
+class error_check_result:
+    syntax_valid: bool
+    semantic_valid: bool
+    errors: list[str]
+
+# output from LLM_checker
+@dataclass 
+class LLM_output:
+    ambiguous: bool
+    ambiguity_reason: str
+    malicious: bool
+    malicious_reason: str
+    confidence: float
+
+# output of final classifier
+@dataclass
+class prediction:
+    label: int
+    confidence: float
+
+# full result of pipeline
+@dataclass
+class final_result:
+    prompt: str
+    sql_query: str
+    ambiguous: bool
+    ambiguity_reason: str
+    malicious: bool
+    malicious_reason: str
+    confidence: float
+    prediction: int
